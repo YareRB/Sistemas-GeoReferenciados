144693be010f88322234206eb0d0fa685257da15
YareliRB <yareram78@gmail.com>
2022-03-19 23:31:14 -0600

    terminó practica 6

---
+++ js/metodos.js
@@ -0,0 +1,47 @@
+var map;
+
+var coordenadas = {
+  lat: 0,
+  lng: 0,
+};
+
+var propiedades = {
+  center: coordenadas,
+  zoom: 20,
+};
+
+function iniciaMapa() {
+  map = new google.maps.Map(document.getElementById("map6"), propiedades);
+
+  var icono = {
+    url: "https://cdn.pixabay.com/photo/2017/01/11/08/31/icon-1971129_960_720.png", //"https://media.giphy.com/media/1ZweHMlSaxcc32mSSu/giphy.gif",
+    scaledSize: new google.maps.Size(50, 50),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(0, 0),
+  };
+
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    icon: icono,
+    map: map,
+  });
+
+  if (navigator.geolocation) {
+    setInterval(() => {
+      moverPosicion(marker);
+    }, 5000);
+  }
+
+  function moverPosicion(marker) {
+    navigator.geolocation.getCurrentPosition((posicion) => {
+      var pos = {
+        lat: posicion.coords.latitude,
+        lng: posicion.coords.longitude,
+      };
+
+      marker.setPosition(pos);
+      map.panTo(pos);
+      map.setCenter(pos);
+    });
+  }
+}
