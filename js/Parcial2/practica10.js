d143d9a185d2190c60c1c82a513cdee88a60e00a
YareliRB <yareram78@gmail.com>
2022-04-03 05:09:46 -0500

    prueba v2

--- js/Parcial2/practica10.js
+++ js/Parcial2/practica10.js
@@ -13,10 +13,10 @@ function iniciaMapa(){
         response.json().then(function(datos){
             const map = new google.maps.Map(document.getElementById('map9'), propiedades);
             datos.forEach(marcador => { 
-                var informacion = '<h1 class="text2">' + marcador.CountryName +'</h1>';
+                var informacion = '<div class="div-center"><h1 class="text2">' + marcador.CountryName +'</h1>';
                 informacion += '<p class="text3"><strong>Capital:</strong> ' + marcador.CapitalName + '</p>';
                 informacion += '<p class="text3"><strong>Continente: </strong>'+ marcador.ContinentName +'</p>';
-                informacion += '<div> <i class="fa-solid fa-earth-africa"></i> </div> <div class="bg-dark"><p class="text3 text-white"><strong>Código de País: </strong>'+ marcador.CountryCode +'</p></div>';                
+                informacion += '<div class="bg-dark"><p class="text3 text-white m-2"><strong>Código de País: </strong>'+ marcador.CountryCode +'</p></div><div> <i class="fa-solid fa-earth-africa"></i> </div></div>';                
                 var infoWindow = new google.maps.InfoWindow({content: informacion});
                 let marker = new google.maps.Marker({
                     map: map,
