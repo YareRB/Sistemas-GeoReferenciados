f0a1c0114bf9bd41d74cb212890f2c601456dcda
YareliRB <yareram78@gmail.com>
2022-04-03 05:23:06 -0500

    prueba v3

--- js/Parcial2/practica10.js
+++ js/Parcial2/practica10.js
@@ -13,10 +13,35 @@ function iniciaMapa(){
         response.json().then(function(datos){
             const map = new google.maps.Map(document.getElementById('map9'), propiedades);
             datos.forEach(marcador => { 
-                var informacion = '<div class="div-center"><h1 class="text2">' + marcador.CountryName +'</h1>';
+                var informacion = '<div class="text-center"><h1 class="text2">' + marcador.CountryName +'</h1>';
                 informacion += '<p class="text3"><strong>Capital:</strong> ' + marcador.CapitalName + '</p>';
                 informacion += '<p class="text3"><strong>Continente: </strong>'+ marcador.ContinentName +'</p>';
-                informacion += '<div class="bg-dark"><p class="text3 text-white m-2"><strong>Código de País: </strong>'+ marcador.CountryCode +'</p></div><div> <i class="fa-solid fa-earth-africa"></i> </div></div>';                
+                informacion += '<div class="bg-dark"><p class="text3 text-white m-2"><strong>Código de País: </strong>'+ marcador.CountryCode +'</p></div>'
+                
+                if(marcador.ContinentName == "Africa"){
+                    informacion += '<div><i class="fa-solid fa-earth-africa fa-3x"></i> </div></div>';                
+                }else{
+                    if(marcador.ContinentName == "Asia"){
+                        informacion += '<div><i class="fa-solid fa-earth-asia fa-3x"></i> </div></div>';                
+                    }else
+                    {
+                        if(marcador.ContinentName == "Central America" || marcador.ContinentName == "North America" || marcador.ContinentName == "South America"){
+                            informacion += '<div><i class="fa-solid fa-earth-americas fa-3x"></i> </div></div>';                
+                        }else{
+                            if(marcador.ContinentName == "Europe"){
+                                informacion += '<div><i class="fa-solid fa-earth-europe fa-3x"></i> </div></div>';                
+                            }
+                            else{
+                                if(marcador.ContinentName == "Australia"){
+                                    informacion += '<div><i class="fa-solid fa-earth-oceania fa-3x"></i> </div></div>';                
+                                }else{
+                                    informacion += '<div><i class="fa-solid fa-globe fa-3x"></i> </div></div>';                
+                                }
+                            }
+                        }
+                    }
+                }
+               
                 var infoWindow = new google.maps.InfoWindow({content: informacion});
                 let marker = new google.maps.Marker({
                     map: map,
